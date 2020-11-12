b4528bd6da926bbac6ae82b47c7fb9f9aa9c9271
Leon Funnell <leon_funnell@epam.com>
2020-11-12 23:57:37 +0000

    Changes to be committed: 	modified:   aws-create-static-website-bash.sh  Untracked files: 	example.sh 	test.sh

--- aws-create-static-website-bash.sh
+++ aws-create-static-website-bash.sh
@@ -7,86 +7,105 @@
 # -ar|--aws-region <AWS_DEFAULT_REGION> (optional - taken from environment unless specified)
 # -a|--application-name <application name> (mandatory)
 # -e|--environment-name <environment name> (mandatory)
-# -l|--language-id <language two letter identifier> (optional)
+# -lang|--language-id <language two letter identifier> (optional)
 # -c|--country-id <country two letter identifier> (optional)
-# <unique instance name> (optional)
+# -i|--instance-name <unique instance name> (optional)
 # -v|--initial-version (optional)
+# -s|--silent (optional - prevent screen output)
+# -l|--logpath <path for logfiles> (optional)
 
 
-for i in "$@"
-do
-case $i in
-    -ak=*=*|--aws-access-key=*)
-    aws_access_key="${i#*=}"
-	export AWS_ACCESS_KEY=$aws_access_key
-    shift # past argument=value
-    ;;
-    -as=*|--aws-secret=*)
-    aws_secret="${i#*=}"
-	export AWS_SECRET_ACCESS_KEY=$aws_secret
-    shift # past argument=value
-    ;;
-    -ar=*|--aws-region =*)
-    aws_region="${i#*=}"
-	export AWS_DEFAULT_REGION=$aws_region
-    shift # past argument=value
-    ;;
-    -a=*|--app-id=*)
-    app_id="${i#*=}"
-    shift # past argument=value
-    ;;
-    -e=*|--environment-name=*)
-    environment_name="${i#*=}"
-    shift # past argument=value
-    ;;
-    -l=*|--language-id=*)
-    language_id="${i#*=}"
-    shift # past argument=value
-    ;;
-    -c=*|--country-id =*)
-    country_id="${i#*=}"
-    shift # past argument=value
-    ;;
-    -i|--instance-name )
-    instance_name="${i#*=}"
-    shift # past argument with no value
-    ;;
-    -v|--initial-version)
-    initial_version="${i#*=}"
-    shift # past argument with no value
-    ;;
-    *)
-          # unknown option
-    ;;
-esac
+
+
+
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -ak|--aws-access-key)
+        aws_access_key=$VALUE
+		export AWS_ACCESS_KEY=$aws_access_key
+            ;;
+        -as|--aws-secret)
+        aws_secret=$VALUE
+        export AWS_SECRET_ACCESS_KEY=$aws_secret
+        ;;
+        -ar|--aws-region)
+        aws_region=$VALUE
+        export AWS_DEFAULT_REGION=$aws_region
+        ;;
+        -a|--application-name)
+        app_id=$VALUE
+        echo "application-name=$app_id"
+        ;;
+        -e|--environment-name)
+        environment_name=$VALUE
+        echo  "environment-name=$environment_name"
+        ;;
+        -lang|--language-id)
+        language_id=$VALUE
+        ;;
+        -c|--country-id)
+        country_id=$VALUE
+        ;;
+        -i|--instance-name)
+        instance_name=$VALUE
+        ;;
+        -v|--initial-version)
+        initial_version=$VALUE
+        ;;
+        -l|--logpath)
+        logpath=$VALUE
+        ;;
+        -s|--silent)
+        silent=true
+        ;;			
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+
 
 # if no region was specified, take it from shell environment if exists, otherwise set to us-east-1 (AWS Default region)
-if [-z "$aws_region"] then
-	if [-z "$AWS_ACCESS_KEY_ID"]
-		then
-			aws_region=$AWS_DEFAULT_REGION
-	else
-		aws-region=us-west-1
-	fi
+if [ ! $aws_region ]; then
+     if [ ! "AWS_DEFAULT_REGION"]; then
+          aws_region=$AWS_DEFAULT_REGION;
+     else
+          aws-region="us-west-1";
+     fi
 fi
 
-projectname=cwebdevleon
 report() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
-  echo "$*"
+	if [ ! $logpath ]; then
+		echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2;
+	fi
+	if [ ! $silent ]; then
+		echo "$*";
+	fi
 }
 
-
-appname="$app_id-$environment_name"
+appname="$app_id"
+appname+="-$environment_name"
+echo "Line 90 - appname=$appname"
 # add optional parameters to appname if they exist
 if ! [-z "$language_id"]; then
-	appname+="-$language_id"
+     appname+="-$language_id";
 fi
 if ! [-z "$country_id"]; then
-	appname+="-$country_id"
+     appname+="-$country_id";
 fi
 if ! [-z "$instance_name"]; then
-	appname+="-$instance_name"
+     appname+="-$instance_name";
 fi
 
 
@@ -102,27 +121,32 @@ cfbucketreadpolicy="$appname-cf-bucketread-policy-user"
 bucketname="$appname-bucket"
 bucketarn="arn:aws:s3:::$bucketname"
 
-if [ ! -d log ]; then
-  mkdir -p log;
+#initialise logpath if specified
+if [ ! $logpath ]; then
+     if [ ! -d $logpath ]; then
+        mkdir -p $logpath;
+    fi
+    outputlog=$logpath/$appname$(date +%Y-%m-%d.%H.%M.%S).log;
 fi
 
-outputlog=log/$appname$(date +%Y-%m-%d.%H.%M.%S).log
-# rm $outputlog
-
-# report output to stdout and log
+# function to report output to stdout and log
 report() {
-	echo $*
-	echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >>$outputlog
+     # echo output to stdout if silent flag not set
+    if [ "$silent" != true ]; then
+        echo $*;
+    fi
+    if [ ! "$logpath" ]; then
+        echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >>$outputlog;
+    fi
 }
 
 # runs command and reports output
-runcommand () { 
-
-	command=$1
-	report "running $command"
-	output=$($command 2>&1)
-	report "$output"
-	}
+runcommand () {
+     command="$*"
+     report "running $command"
+     output="$($command 2>&1)"
+     report "$output"
+     }
 
 # report variables
 report "app_id=$app_id"
@@ -141,7 +165,7 @@ report "cfmodifyuser=$cfmodifyuser"
 report "bucketreadoai=$bucketreadoai"
 report "jenkinsbucketwriteuser=$jenkinsbucketwriteuser"
 report "bucketname=$bucketname"
-report "initial_version=$initial_version
+report "initial_version=$initial_version"
 
 # create S3 Bucket
 runcommand="aws s3api create-bucket --bucket $bucketname --region $aws_region --create-bucket-configuration LocationConstraint=$aws_region ----acl private"
@@ -164,7 +188,7 @@ read -r -d '' bucketwritepolicydoctemplate << EOM
             "Action": [
                 "s3:DeleteObject**",
                 "s3:ListObject*"
-				"s3:PutObject**"
+                    "s3:PutObject**"
             ],
             "Resource": [
                 "arn:aws:s3:::mybucket/*"
@@ -173,31 +197,16 @@ read -r -d '' bucketwritepolicydoctemplate << EOM
     ]
 }
 EOM
+
 #substitute mybucket for $bucketname
 bucketwritepolicydoc="${bucketwritepolicydoctemplate/mybucket/$bucketname}"
-report "bucketwritepolicydoc=$bucketwritepolicydoc" 
+report "bucketwritepolicydoc=$bucketwritepolicydoc"
 
 # Create Origin Access ID for S3 bucket
-	# Create Origin
-	
+# Create Origin
+
 # Create Cloudfront Blue Distribution
-	# Origin=$bucketname.s3.amazonaws.com
-	# OriginPath=
+# Origin=$bucketname.s3.amazonaws.com
+# OriginPath=
 # Create Cloudfront Green Distribution
 # Create Cloudfront Test Distribution
-
-
-
-
-
-
-
-output=$(aws iam create-role --role-name $bucketrole --assume-role-policy-document file://$bucketpolicydoc)
-echo $(date +%Y-%m-%d.%H.%M.%S) output=$output>>$outputlog
-roleArn=$output
-echo $(date +%Y-%m-%d.%H.%M.%S)    Creating IAM User>>$outputlog
-output=$(aws iam create-user --user-name $bucketuser  --permissions-boundary $roleArn)
-echo $(date +%Y-%m-%d.%H.%M.%S) output=$output>>$outputlog
-
-
-
